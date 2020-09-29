add89d5c34d2e7161b52c1657319f40473c5a4bd
Matt Daniels <matthew.f.daniels@gmail.com>
2020-09-28 20:51:17 -0400

    first build

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,8 +1,36 @@
+import Swiper from 'swiper';
+import noUiSlider from 'nouislider'
+
+let mySwiper = null;
 /* global d3 */
 function resize() {}
 
+function swiperController(){
+  d3.select(".start-slide").select(".black-button").on("click",function(d){
+    mySwiper.slideNext();
+  });
+}
+
 function init() {
-  console.log('Make something awesome!');
+
+  mySwiper = new Swiper ('.swiper-container', {
+    slidesPerView:1,
+    simulateTouch:false,
+    touchStartPreventDefault:false,
+    allowTouchMove:false,
+  })
+
+  noUiSlider.create(d3.select("#age-slider").node(), {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+  });
+
+  swiperController();
+
 }
 
 export default { init, resize };
