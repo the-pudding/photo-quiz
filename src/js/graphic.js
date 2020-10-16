33e865a46643fc63ad4d80bcbf95241be8e2503b
Jan Diehm <jadiehm@gmail.com>
2020-10-16 15:07:56 -0500

    closing a bunch if issues

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -82,7 +82,6 @@ function swiperController(){
   });
 
   $resultsButton.on('click', function(d) {
-    console.log('clicked')
     mySwiper.slideTo(7)
     d3.select('.photo-bg').style('display', 'none')
     d3.select(".all-done").style("height","auto")
@@ -90,7 +89,7 @@ function swiperController(){
     buildFinalSlide()
   })
 
-  sliderAge.on('start', function(d) {
+  sliderAge.on('change', function(d) {
     $ageButton.classed('is-disabled', false)
     d3.selectAll('.noUi-handle').style('animation-play-state', 'paused')
   })
@@ -123,47 +122,30 @@ function swiperController(){
   })
 }
 
-function buildResults(data, type) {
-  console.log(data, type)
+function buildResults(data, belongsTo) {
+  let container = d3.select(`.photo-answer-wrapper-${belongsTo}`);
 
-  
-
-}
-
-function buildFinalSlide(){
-  console.log("building final slide")
-  let container = d3.select(".photo-answer-wrapper-your");
-  let results = output;
-  const others = d3.range(10).map(d => {
-    const id = `${d + 1}`;
-    const match = output.find(o => o.id === id);
-    if (!match) return { id };
-    return null;
-  }).filter(d => d);
-
-
-  let photoAnswers = container.selectAll("div").data(results).enter().append("div").attr("class","photo-answer");
+  let photoAnswers = container.selectAll("div").data(data).enter().append("div").attr("class","photo-answer");
   let photoAnswerTitleWrapper = photoAnswers.append("div").attr("class","photo-answer-title-wrapper");
 
-  // BUILD YOUR RESULTS
-  buildResults(results, "yours")
-  buildResults(others, "others")
+  // Adds photo titles
+  if (belongsTo == 'yours') {
+    photoAnswerTitleWrapper.append("p").attr("class","photo-answer-title").html(function(d,i){ return "Photo No. <span>"+(i+1)+"</span>"; })
 
-  // TODO BUILD ALL RESULTS
-  photoAnswerTitleWrapper.append("p").attr("class","photo-answer-title").html(function(d,i){ return "Photo No. <span>"+(i+1)+"</span>"; })
+    photoAnswerTitleWrapper.append("div").datum(function(d,i){ return i; })
+      .attr("class","photo-answer-key")
+      .selectAll("div")
+      .data(data)
+      .enter()
+      .append("div")
+      .attr("class","photo-answer-key-box")
+      .classed("photo-answer-key-box-active",function(d,i){
+        if(i==d3.select(this.parentNode).datum()){ return true }
+        return false;
+      });
+  }
 
-  photoAnswerTitleWrapper.append("div").datum(function(d,i){ return i; })
-    .attr("class","photo-answer-key")
-    .selectAll("div")
-    .data(results)
-    .enter()
-    .append("div")
-    .attr("class","photo-answer-key-box")
-    .classed("photo-answer-key-box-active",function(d,i){
-      if(i==d3.select(this.parentNode).datum()){ return true }
-      return false;
-    });
-
+  // Adds a color and bw row for each photo
   let photoAnswersRow = photoAnswers.append("div")
     .attr("class","photo-answer-row-wrapper")
     .selectAll("div")
@@ -177,14 +159,19 @@ function buildFinalSlide(){
     .attr("class","photo-answer-row")
     .classed("your-guess-row",function(d){
       let colorQuizzed = +d3.select(this.parentNode).datum().color;
-      if(d[0] == colorQuizzed){ return true; }
+      if(belongsTo == 'yours' && d[0] == colorQuizzed){ return true; }
+      return false;
+    })
+    .classed("color-row",function(d){
+      if (belongsTo == 'others' && d[0] == 1) { return true }
       return false;
     })
     .classed("bw-row",function(d){
       if(d[0] == 0){ return true; }
       return false;
     });
-
+  
+  // Calculate and display average text 
   let photoAnswerText = photoAnswersRow.append("p").attr("class","photo-answer-text-wrapper")
   let averagesCalculated = new Map();
 
@@ -221,9 +208,9 @@ function buildFinalSlide(){
       return "People who saw this photo in black and white dated it <span>"+(diffVal)+" years "+change+"</span> than people who saw it in color."
     }
     return null;
-
   })
 
+  // Append photos
   photoAnswersRow.append("div").attr("class","photo-answer-photo").style("background-image",function(d,i){
     let rowData = d3.select(this.parentNode.parentNode.parentNode).datum();
     let fileName = colorCrossWalk[d[0]];
@@ -236,8 +223,8 @@ function buildFinalSlide(){
     return false;
   })
 
+  // Append sliders
   let photoAnswerSliderContainer = photoAnswersRow.append("div").attr("class","photo-answer-slider-container")
-
   let photoAnswerSlider = photoAnswerSliderContainer.append("div").attr("class","photo-answer-slider").each(function(d){
 
   let orient = "horizontal";
@@ -284,9 +271,12 @@ function buildFinalSlide(){
       return null;
     })
 
+  // Append dots
   let dotWrapper = photoAnswerSlider.append("div").attr("class","dot-wrapper");
 
-  let dotYou = dotWrapper
+  // Append your dot
+  if (belongsTo == 'yours') {
+    let dotYou = dotWrapper
     .filter(function(d){
       if(d3.select(this.parentNode.parentNode.parentNode).classed("your-guess-row")){ return d; };
     })
@@ -308,13 +298,15 @@ function buildFinalSlide(){
     })
     ;
 
-  dotYou.append("p").html(function(d){
-    let you = +d3.select(this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode).datum().selected;
-    if(+you < 20){ you = (+you)+2000; }
-    else { you = +you + 1900; }
-    return "<span>You</span>"+you;
-  })
+    dotYou.append("p").html(function(d){
+      let you = +d3.select(this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode).datum().selected;
+      if(+you < 20){ you = (+you)+2000; }
+      else { you = +you + 1900; }
+      return "<span>You</span>"+you;
+    })
+  }
 
+  // Apended all faded dots
   dotWrapper.append("div").attr("class","faded-dots-wrapper")
     .selectAll("div")
     .data(function(d){ return d[1] })
@@ -338,30 +330,30 @@ function buildFinalSlide(){
       return null;
     })
 
+  // Append average dot
   let dotAvg = dotWrapper.append("div")
-    .attr("class","dot-big dot-avg")
-    .style("left",function(d){
-      if(vw < 751){ return null; }
+  .attr("class","dot-big dot-avg")
+  .style("left",function(d){
+    if(vw < 751){ return null; }
+    let avg = Math.floor(d3.mean(d[1],function(d){
+      let year = 1900;
+      if(+d.selected < 20){ return +d.selected+2000; }
+      return +d.selected + 1900;
+    }));
+    return scale(avg)+"%";
+  })
+  .style("top",function(d){
+    if(vw < 751){
       let avg = Math.floor(d3.mean(d[1],function(d){
         let year = 1900;
         if(+d.selected < 20){ return +d.selected+2000; }
         return +d.selected + 1900;
       }));
       return scale(avg)+"%";
-    })
-    .style("top",function(d){
-      if(vw < 751){
-        let avg = Math.floor(d3.mean(d[1],function(d){
-          let year = 1900;
-          if(+d.selected < 20){ return +d.selected+2000; }
-          return +d.selected + 1900;
-        }));
-        return scale(avg)+"%";
-      }
-      return null;
+    }
+    return null;
 
-    })
-    ;
+  });
 
   dotAvg.append("p").html(function(d){
     let avg = Math.floor(d3.mean(d[1],function(d){
@@ -370,11 +362,12 @@ function buildFinalSlide(){
       return +d.selected + 1900;
     }));
     return "<span>Avg</span>"+avg;
-  })
+  })  
 
+  // Append actual data
   let secondRow = photoAnswersRow.filter(function(d,i){
-      if(i!=0){ return d; }
-    })
+    if(i!=0){ return d; }
+  })
 
   secondRow
     .select(".actual-bar").append("div")
@@ -387,36 +380,34 @@ function buildFinalSlide(){
       let actualDate = actualDates.get(+d3.select(this.parentNode.parentNode.parentNode.parentNode).datum().id);
       return '<p>'+actualDate+'</p><p>Actual date taken</p>'
     });
+  
+  // Change intro sentence
+  if (belongsTo == 'yours') {
+    d3.select(".yourColorspace").text(function(d) {
+      if (colorCrossText[+data[0].color] == 'color') { return `in ${colorCrossText[+data[0].color]}` }
+      else { return `digitally altered ${colorCrossText[+data[0].color]}` }
+    });
+  
+    d3.select(".otherColorspace").text(function(){
+      let color = +data[0].color;
+      if(color == 1){ return `digitally altered ${colorCrossText[color-1]}` }
+      return colorCrossText[color+1];
+    });
+  }
+}
 
-  d3.select(".yourColorspace").text(function(d) {
-    if (colorCrossText[+results[0].color] == 'color') { return `in ${colorCrossText[+results[0].color]}` }
-    else { return `digitally altered ${colorCrossText[+results[0].color]}` }
-  });
-  d3.select(".yourYear").text(function(d){
-    let year = +results[0].selected;
-    if(year < 20){ return 2000+year; }
-    return 1900+year;
-  });
+function buildFinalSlide(){
+  //let container = d3.select(".photo-answer-wrapper-your");
+  let results = output;
+  const others = d3.range(10).map(d => {
+    const id = `${d + 1}`;
+    const match = output.find(o => o.id === id);
+    if (!match) return { id };
+    return null;
+  }).filter(d => d);
 
-  d3.select(".yearDiff").text(function(d){
-    let year = +results[0].selected;
-    if(year < 20){ return Math.abs(actualDates.get(+results[0].id) - (2000+year)) + " years"; }
-    return Math.abs(actualDates.get(+results[0].id) - (1900+year)) + " years";
-  });
-
-  d3.select(".actualYear").text(actualDates.get(+results[0].id));
-
-  d3.select(".avgYear").text(function(d){
-    let year = +results[0].selected;
-    if(year < 20){ return Math.abs(averagesCalculated.get(+results[0].id+"_"+results[0].color) - (2000+year)) + " years"; }
-    return Math.abs(averagesCalculated.get(+results[0].id+"_"+results[0].color) - (1900+year)) + " years";
-  });
-
-  d3.select(".otherColorspace").text(function(){
-    let color = +results[0].color;
-    if(color == 1){ return `digitally altered ${colorCrossText[color-1]}` }
-    return colorCrossText[color+1];
-  });
+  buildResults(results, "yours")
+  buildResults(others, "others")
 }
 
 function setupDB() {
@@ -582,7 +573,7 @@ function init(data) {
     });
     d = slider;
 
-    el.noUiSlider.on('start', function(d) {
+    el.noUiSlider.on('change', function(d) {
       d3.selectAll('.swiper-slide-active').select('.photo-submit').classed('is-disabled', false)
       d3.selectAll('.noUi-handle').style('animation-play-state', 'paused')
     })
